07ca71309a3fa7b0c0d896fd2d2dcf7f5c5b491c
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-08-20 09:58:05 +0200

    Updated dependencies

--- src/Bridge/Symfony/DependencyInjection/TranslationAdapterLocoExtension.php
+++ src/Bridge/Symfony/DependencyInjection/TranslationAdapterLocoExtension.php
@@ -30,7 +30,7 @@ class TranslationAdapterLocoExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration($container);
+        $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
         $domainToProjectMap = [];
